fdc4511b6041467f1371b374ad3f0820a305f0e9
hsmtkk <hsmtkk@gmail.com>
2020-07-05 07:52:07 +0900

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 export DOCKER_BUILDKIT=1
-for n in ubuntu_build-20.04 openssl_build-1.1.1g squid_build-5.0.3 squid-5.0.3 squid_build-latest squid-latest
+for n in ubuntu_build-20.04 openssl_build-1.1.1g squid_build-4.12 squid_build-5.0.3 squid-5.0.3 squid_build-latest squid-4.12 squid-5.0.3 squid-latest
 do
   cd $n
     docker build --tag hsmtkk/squid_docker:$n .
