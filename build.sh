34a7cbe587269aff7142672c217d50804434e2ef
hsmtkk <hsmtkk@gmail.com>
2020-07-04 19:04:18 +0900

    switch from centos to ubuntu

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-export DOCKER_BUILDKIT=1
-for n in centos-7 centos_devel-7 openssl_build-1.1.1g squid-5.0.3 squid_build-5.0.3 squid_build-latest squid-latest
+#export DOCKER_BUILDKIT=1
+for n in ubuntu-20.04 ubuntu_build-20.04 openssl_build-1.1.1g squid_build-5.0.3 squid-5.0.3 squid_build-latest squid-latest
 do
   cd $n
     docker build --tag hsmtkk/squid_docker:$n .
